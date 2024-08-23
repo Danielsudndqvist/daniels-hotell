71a0eac30cc92b6bb78cfcc0148815b136e993d4
Daniel Sundqvist <1891sunkan@gmail.com>
2024-08-23 12:33:30 +0000

    first model added-room

---
+++ hotel_booking_project/rooms/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Room(models.Model):
+    ROOM_TYPES = [
+        ('DLX', 'Deluxe'),
+        ('SLDX', 'Super Deluxe'),
+        # Add more room types as needed
+    ]
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    room_type = models.CharField(max_length=3, choices=ROOM_TYPES)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
