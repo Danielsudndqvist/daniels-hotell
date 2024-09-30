7f561534d8a7140e5dc9d6992ac55059eeb99891
Daniel Sundqvist <1891sunkan@gmail.com>
2024-09-30 14:28:32 +0000

    depolyment error fix

--- daniels-hotell/wsgi.py
+++ daniels-hotell/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hotel_booking_project project.
+WSGI config for daniels-hotell project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_booking_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'daniels-hotell.settings')
 
 application = get_wsgi_application()
