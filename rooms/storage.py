d49222528cb9e79ae9c48f9bf03d3a6d76d0f646
Daniel Sundqvist <1891sunkan@gmail.com>
2025-05-04 19:34:47 +0000

    Add GoogleCloudStaticFileStorage class

--- rooms/storage.py
+++ rooms/storage.py
@@ -1,17 +1,17 @@
 from storages.backends.gcloud import GoogleCloudStorage
-from storages.utils import setting
-
 
 class GoogleCloudMediaFileStorage(GoogleCloudStorage):
     """Google Cloud Storage class for media files."""
-
-    bucket_name = setting("GS_BUCKET_NAME")
-    location = "media"  # Store files in a 'media' subdirectory
-
+    
     def __init__(self, *args, **kwargs):
-        kwargs["default_acl"] = "publicRead"  # Make files publicly accessible
+        kwargs.update({"location": "media"})
+        # No predefinedAcl parameter for uniform bucket-level access
         super().__init__(*args, **kwargs)
 
-    def url(self, name):
-        """Return the URL for accessing the file."""
-        return super().url(name)
+class GoogleCloudStaticFileStorage(GoogleCloudStorage):
+    """Google Cloud Storage class for static files."""
+    
+    def __init__(self, *args, **kwargs):
+        kwargs.update({"location": "static"})
+        # No predefinedAcl parameter for uniform bucket-level access
+        super().__init__(*args, **kwargs)
